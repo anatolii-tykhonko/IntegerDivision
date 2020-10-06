d0e4fe90d49898cf2b5191f5d758e43927bec4b3
Dranatom <zorg.anatom1990@gmail.com>
2020-10-06 16:53:09 +0300

    added new tests

--- src/test/java/ua/com/foxminded/DivisionTest.java
+++ src/test/java/ua/com/foxminded/DivisionTest.java
@@ -7,8 +7,69 @@ import static org.junit.jupiter.api.Assertions.*;
 class DivisionTest {
     Division test = new Division();
 
+
     @Test
-    void printResultWhenDivisorContentTwoDigit(){
+    void throwIllegalArgumentExceptionWhenDivisorZero() {
+        int inputDividend = 12345;
+        int inputDivisor = 0;
+
+        assertThrows(IllegalArgumentException.class,
+                () -> test.divineTwoPositiveIntegers(inputDividend, inputDivisor));
+    }
+
+    @Test
+    void throwIllegalArgumentExceptionWhenDividendNegativeDivisorPositive(){
+        int inputDividend = -123;
+        int inputDivisor = 12;
+
+        assertThrows(IllegalArgumentException.class,
+                () -> test.divineTwoPositiveIntegers(inputDividend, inputDivisor));
+    }
+
+    @Test
+    void throwIllegalArgumentExceptionWhenDividendPositiveDivisorNegative(){
+        int inputDividend = 25;
+        int inputDivisor = -2;
+
+        assertThrows(IllegalArgumentException.class,
+                () -> test.divineTwoPositiveIntegers(inputDividend, inputDivisor));
+    }
+
+    @Test
+    void throwIllegalArgumentExceptionWhenDividendNegativeDivisorNegative(){
+        int inputDividend = -9;
+        int inputDivisor = -3;
+
+        assertThrows(IllegalArgumentException.class,
+                () -> test.divineTwoPositiveIntegers(inputDividend, inputDivisor));
+    }
+
+    @Test
+    void returnOneLineStringWhereDividendDivisorEqualsZeroWhenDivisorBiggerDividend() {
+        int inputDividend = 0;
+        int inputDivisor = 2;
+        String expected = "0 / 2 = 0";
+
+        String result = test.divineTwoPositiveIntegers(inputDividend, inputDivisor);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void returnOnlyFourLinesWhenDividendEqualsDivisor(){
+        int inputDividend = 9;
+        int inputDivisor = 9;
+        String expected =   "_9|9\n" +
+                            " 9|-\n" +
+                            " -|1\n" +
+                            " 0\n";
+
+        String result = test.divineTwoPositiveIntegers(inputDividend, inputDivisor);
+
+        assertEquals(expected, result);
+    }
+    @Test
+    void returnResultWhenDivisorBiggerThatFirstDigitOfDividend() {
         int inputDividend = 12345;
         int inputDivisor = 13;
         String expected =   "_12345|13\n" +
@@ -21,7 +82,9 @@ class DivisionTest {
                             "   117\n" +
                             "   ---\n" +
                             "     8\n";
-        String result = test.makeDivision(inputDividend, inputDivisor);
+
+        String result = test.divineTwoPositiveIntegers(inputDividend, inputDivisor);
+
         assertEquals(expected, result);
     }
 
